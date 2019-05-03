b4426b9ff83db5917053961676a527e57ecf3184
Naim Sakaamini <naimsakaamini@naims-macbook-pro.local>
2019-05-03 15:16:08 -0400

    Added the add to-do functionality

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["one", "two", "three"]
+    var itemArray = ["one", "two", "three"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,8 +42,29 @@ class ToDoListViewController: UITableViewController {
         }
 
         tableView.deselectRow(at: indexPath, animated: true)
-        
     }
 
+    //MARK -  Add New Items
+    
+    @IBAction func addBurronPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New To-Do Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Add new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 

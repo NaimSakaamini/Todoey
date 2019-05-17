1fd2213f4d6ffafb03e1be84c16f34ce7ffcc6a7
Naim Sakaamini <naimsakaamini@naims-macbook-pro.local>
2019-05-17 12:46:57 -0400

    convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Naim Sakaamini on 2019-05-17.
+//  Copyright © 2019 Naim Sakaamini. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var title : String = ""
+    var done : Bool = false
+}
